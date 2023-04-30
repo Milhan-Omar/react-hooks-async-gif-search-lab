df7c8d90c3c2f46b3f8c5506edb5f66425e95786
Milhan Osman <milhanomarosman@gmail.com>
2023-04-30 16:54:13 +0300

    completed assigment

---
+++ src/components/GifList.js
@@ -0,0 +1,12 @@
+function GifList({ gifsList }) {
+  console.log(gifsList);
+  const ulContent = gifsList.map((element) => {
+    return (
+      <li key={element.id}>
+        <img src={element.images.original.url} />
+      </li>
+    );
+  });
+  return <ul>{ulContent}</ul>;
+}
+export default GifList;
